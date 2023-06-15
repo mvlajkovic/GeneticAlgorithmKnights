f82c5a56624b548c9fa1f03ade63031a48b13cb9
Milica <Milica@DESKTOP-8K36UN1>
2023-06-15 23:50:44 +0200

    User Story no 3. Commit: Changes to the generation of random individual. Implemented crossover operations.

--- src/genal/ChessBoard.java
+++ src/genal/ChessBoard.java
@@ -4,6 +4,7 @@
  */
 package genal;
 
+import java.util.Arrays;
 import java.util.Random;
 
 /**
@@ -21,17 +22,21 @@ public class ChessBoard implements GeneticAlgorithm {
 
     //target, gene pool?
     public ChessBoard() {
-        int counter = 0;
+        //create empty board
         for (int i = 0; i < length; i++) {
             for (int j = 0; j < length; j++) {
-                if (counter < numOfKnights) {
-                    board[i][j] = (int) Math.round(Math.random());
-                    if (board[i][j] == 1) {
-                        counter++;
-                    }
-                } else {
-                    board[i][j] = 0;
-                }
+                board[i][j] = 0;
+            }
+        }
+        Random random = new Random();
+        int counter = 0;
+        //set knights at random places
+        while (counter < numOfKnights) {
+            int i = random.nextInt(length);
+            int j = random.nextInt(length);
+            if (board[i][j] == 0) {
+                board[i][j] = 1;
+                counter++;
             }
         }
     }
@@ -44,7 +49,7 @@ public class ChessBoard implements GeneticAlgorithm {
     private static boolean isValidPosition(int row, int col, int boardSize) {
         return (row >= 0 && row < boardSize && col >= 0 && col < boardSize);
     }
-    
+
     @Override
     public double fitness() {
         //this one should calculate the number of attacks
@@ -65,7 +70,7 @@ public class ChessBoard implements GeneticAlgorithm {
                     }
                 }
             }
-           // System.out.println("total after row " + i + ": " + attackedCount);
+            // System.out.println("total after row " + i + ": " + attackedCount);
         }
         return attackedCount;
 
@@ -73,7 +78,50 @@ public class ChessBoard implements GeneticAlgorithm {
 
     @Override
     public GeneticAlgorithm crossover(GeneticAlgorithm obj) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        ChessBoard ref = (ChessBoard) obj;
+        int counter = 0;
+        int[][] cross = new int[this.length][this.length];
+        Random rand = new Random();
+        //go through the board and set parent values while noOfKnights not reached
+        for (int i = 0; i < this.length; i++) {
+            for (int j = 0; j < this.length; j++) {
+                if (counter < this.numOfKnights) {
+                    //this will prevent from having more than required num of knights
+                    if (rand.nextBoolean()) {
+                        //we take at random values from first or second board
+                        cross[i][j] = this.board[i][j];
+
+                        counter += this.board[i][j];
+
+                    } else {
+                        cross[i][j] = ref.board[i][j];
+                        counter += ref.board[i][j];
+                    }
+                }
+                else {
+                    cross[i][j] = 0;
+                }
+            }
+        }
+        System.out.println("matrix so far");
+        System.out.println(Arrays.deepToString(cross));
+        System.out.println("counter "+ counter);
+        //what happens if we have less than required num of knights?
+        //well we will replace random zeroes with 1
+        while (counter < numOfKnights) {
+            int i = rand.nextInt(length);
+            System.out.println("i " + i);
+            int j = rand.nextInt(length);
+            System.out.println("j " + j);
+            if (cross[i][j] == 0) {
+                cross[i][j] = 1;
+                counter++;
+                
+            }
+            System.out.println("counter " +counter );
+        }
+        ChessBoard tmp = new ChessBoard(cross);
+        return tmp;
     }
 
     @Override
@@ -81,6 +129,19 @@ public class ChessBoard implements GeneticAlgorithm {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
 
-
+    @Override
+    public String toString() {
+        int num=0;
+        String tmp = "ChessBoard{\n";
+        for (int i = 0; i < length; i++) {
+            for (int j = 0; j < length; j++) {
+                tmp += board[i][j] + " ";
+                num += board[i][j];
+            }
+            tmp += "\n";
+        }
+        tmp += "} with fitness: " + fitness() + " num of knights: " + num;
+        return tmp;
+    }
 
 }
--- src/genal/Main.java
+++ src/genal/Main.java
@@ -14,7 +14,30 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        int[][] board = {{0, 1, 1, 1, 0, 1, 0, 1}, {1, 0, 1, 1, 0, 0, 0, 0}, {1, 1, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0}};
+        ChessBoard instance1 = new ChessBoard(board);
+        double expectedRes = 14.0;
+        double result = instance1.fitness();
+        System.out.println("Testing function fitness: " + (expectedRes == result));
+
+        ChessBoard instance2 = new ChessBoard();
+        ChessBoard instance3 = new ChessBoard();
+        ChessBoard instance4 = new ChessBoard();
+        ChessBoard instance5 = new ChessBoard();
+        ChessBoard instance6 = new ChessBoard();
+        System.out.println(instance3.toString());
+        System.out.println(instance2.toString());
+        System.out.println(instance3.toString());
+        System.out.println(instance4.toString());
+        System.out.println(instance5.toString());
+        System.out.println(instance6.toString());
+
+        System.out.println(((ChessBoard) instance1.crossover(instance2)).toString());
+        System.out.println(((ChessBoard) instance3.crossover(instance2)).toString());
+        System.out.println(((ChessBoard) instance4.crossover(instance2)).toString());
+        System.out.println(((ChessBoard) instance5.crossover(instance2)).toString());
+        System.out.println(((ChessBoard) instance6.crossover(instance2)).toString());
+
     }
-    
+
 }
