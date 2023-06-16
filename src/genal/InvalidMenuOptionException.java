b01b2623bd3c6b0ce2cf3fcf1c24f2288393bc7a
Milica <Milica@DESKTOP-8K36UN1>
2023-06-16 17:27:51 +0200

    User Story no 9. Commit: Define the soulution for B. Define individuals, define a function for generating radnom individual:  Implement fitness, crossover and mutation fuction.Elitism mechanism developed for getting the best of every generation, and the best overall. Enabled user input for knights, and logic for deciding which option to run.

---
+++ src/genal/InvalidMenuOptionException.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package genal;
+
+/**
+ *
+ * @author Milica
+ */
+public class InvalidMenuOptionException extends Exception{
+    
+    public InvalidMenuOptionException(String message) {
+        super(message);
+    }
+    
+}
--- src/genal/Main.java
+++ src/genal/Main.java
@@ -4,6 +4,9 @@
  */
 package genal;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /**
  *
  * @author Milica
@@ -14,15 +17,35 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        int knights = 12;
-        int noOfIndividuals = 1000;
-        int noOfGen = 3000;
+        //int n = 12;
+        int noOfIndividuals = 100;
+        int noOfGen = 1000;
         double mutationRate = 0.25;
-        //optionA(knights, noOfIndividuals, noOfGen, mutationRate);
-        optionB(noOfIndividuals, noOfGen, mutationRate);
 
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("***************************************************************************************************************");
+        System.out.println("Enter number of knights: ");
+        String str = scan.nextLine();
+        if (str == null || !str.matches("\\d+")) {
+            optionC(noOfIndividuals, noOfGen, mutationRate);
+        } else {
+            int i = Integer.parseInt(str);
+            if (i == 0) {
+                optionC(noOfIndividuals, noOfGen, mutationRate);
+            } else if (i > 64) {
+                optionA(64, noOfIndividuals, noOfGen, mutationRate);
+            } else {
+                optionA(i, noOfIndividuals, noOfGen, mutationRate);
+            }
+
+            //optionA(n, noOfIndividuals, noOfGen, mutationRate);
+            //optionB(noOfIndividuals, noOfGen, mutationRate);
+            //optionC(noOfIndividuals, noOfGen, mutationRate);
+        }
+
+    
     }
-
     public static void optionA(int knights, int noOfIndividuals, int noOfGen, double mutationRate) {
         BestMatchA theBest = new BestMatchA();
         //System.out.println("FIRST GENERATION");
@@ -93,8 +116,7 @@ public class Main {
                     if (bm.getMatch().getNumOfKnights() > theBest.getMatch().getNumOfKnights()) {
                         theBest = bm;
                     }
-                }
-                else{
+                } else {
                     theBest = bm;
                 }
             }
@@ -103,12 +125,73 @@ public class Main {
             pop = new Population(gen, mutationRate);
         }
         System.out.println("BEST OVERALL");
-        if(theBest!= null){
-        System.out.println(theBest.toString());}
-        else {
+        if (theBest != null) {
+            System.out.println(theBest.toString());
+        } else {
             System.out.println("No solution");
         }
 
+    }
+
+    public static void optionC(int noOfIndividuals, int noOfGen, double mutationRate) {
+        ArrayList<BestMatchA> selected = new ArrayList<>();
+        ArrayList<Integer> noOfKnights = new ArrayList<>();
+        boolean fit = true;
+        int knights = 1;
+        while (fit) {
+            BestMatchA tmp = bestForNKnights(knights, noOfIndividuals, noOfGen, mutationRate);
+            if (tmp.getMatch().fitness() == 1.0) {
+                selected.add(tmp);
+                noOfKnights.add(knights);
+                knights++;
+            } else {
+                fit = false;
+            }
+        }
+        //the last added element will have the highest number of knights
+        if (selected.isEmpty()) {
+            System.out.println("There is no solution");
+        } else {
+            System.out.println("FINAL SOLUTION");
+            System.out.println("Number of knigths is: " + noOfKnights.get(noOfKnights.size() - 1));
+            System.out.println("EXAMPLE OF SOLUTION");
+            System.out.println(selected.get(selected.size() - 1).toString());
+        }
+
+    }
+
+    public static BestMatchA bestForNKnights(int knights, int noOfIndividuals, int noOfGen, double mutationRate) {
+        System.out.println("*******************************");
+        System.out.println("KNIGHTS: " + knights);
+        System.out.println("*******************************");
+        BestMatchA theBest = new BestMatchA();
+        ChessBoardA[] boards = new ChessBoardA[noOfIndividuals];
+        GeneticAlgorithm[] gen = new GeneticAlgorithm[noOfIndividuals];
+        for (int i = 0; i < noOfIndividuals; i++) {
+            boards[i] = new ChessBoardA(knights);
+            gen[i] = boards[i];
+            //System.out.println(boards[i].toString());
+        }
+        //temp setting the first bord to be the best.
+        theBest.setMatch((ChessBoardA) gen[0]);
+        theBest.setIndividual(0);
+        theBest.setGeneration(0);
+        //System.out.println("NEXT GENERATIONS");
+        for (int i = 0; i < noOfGen; i++) {
+            Population pop = new Population(gen, mutationRate);
+            //System.out.println("GENERATION " + i);
+            BestMatchA bm = pop.getTheBestA();
+            bm.setGeneration(i);
+            //System.out.println(bm.toString());
+            if (bm.getMatch().fitness() > theBest.getMatch().fitness()) {
+                theBest = bm;
+            }
+            //get next gen
+            gen = pop.newGeneration();
+        }
+        System.out.println("BEST OVERALL");
+        System.out.println(theBest.toString());
+        return theBest;
     }
 
 }
